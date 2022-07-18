44ecd8dd1c1c5eb3a89fa531cc1ee8d545ba040f
jromatowski <joshuaromatowski@gmail.com>
2022-07-18 09:26:02 -0700

    buttons work

---
+++ src/components/Welcome.js
@@ -0,0 +1,14 @@
+import Breakfast from "./Breakfast"
+import Dinner from "./Dinner"
+import Lunch from "./Lunch"
+import { Route, Routes, Link } from 'react-router-dom';
+
+function Welcome() {
+    return(
+        <>  
+        Welcome Page Component
+        </>
+    )
+}
+
+export default Welcome
