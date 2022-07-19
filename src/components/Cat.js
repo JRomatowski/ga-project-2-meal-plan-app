6db1ec4a38825d9f29262a9d670bdfb5004e08bb
jromatowski <joshuaromatowski@gmail.com>
2022-07-19 10:21:15 -0700

    got rid of image loading icon, updated readme

--- src/components/Cat.js
+++ src/components/Cat.js
@@ -1,17 +1,26 @@
 import "./Meals.css"
-import { useState, useEffect } from 'react'
+import { useState, useEffect, useRef } from 'react'
+
 
 function Cat() {
 
     const [catPhoto, setCatPhoto] = useState('')
+    const [loading, setLoading] = useState(true);
+    const counter = useRef(0);
 
     useEffect(() => {
         const url = "https://shibe.online/api/cats"
+
+
         fetch(url)
             .then((res) => {
                 return res.json()
             }) 
             .then((data) => {
+                counter.current += 1;
+                if (counter.current >= data.length) {
+                    setLoading(false);
+                }
                 console.log(data)
                 setCatPhoto(data[0])
             })
@@ -37,7 +46,7 @@ function Cat() {
         {/* <button onClick={handleClick}>Cat friend!</button> */}
         {/* <ImageContainer img={catPhoto} msg='' id='cat-box' /> */}
         <h3 id='picture-message'>"ClIcK On ReCiPeS AbOvE!!"</h3>
-        <div className='double-container'>
+        <div style={{visibility: loading ? "hidden" : "visible"}} className='double-container'>
             <div className='image-container'>
                 <img src={catPhoto} alt='food or cat' id='cat-photo'/>
             </div>
--- src/components/Recipes.js
+++ src/components/Recipes.js
@@ -1,9 +1,11 @@
-import { useState, useEffect } from 'react'
+import { useState, useEffect, useRef } from 'react'
 import ImageContainer from './ImageContainer'
 
 function Recipes() {
 
     const [recipe, setRecipe] = useState({})
+    const [loading, setLoading] = useState(true);
+    const counter = useRef(0);
 
     useEffect(() => {
         const url = "https://www.themealdb.com/api/json/v1/1/random.php"
@@ -12,7 +14,16 @@ function Recipes() {
                 return res.json()
             }) 
             .then((data) => {
-                console.log(data)
+                let newData = data.meals[0]
+                // I have no idea why this works, but I found it on stack overflow.  This is different than how I had it on ./Cat.js because the item being returned with the fetch is different, not entirely clear on why.  This API gives an object, and the cat API gives an array?  
+                counter.current += 1;
+                if (counter.current <= newData.idMeal) {
+                    setLoading(false);
+                }
+                // console.log(data)
+                // console.log(data.meals[0])
+                // console.log(data.length)
+                // console.log(newData.idMeal)
                 setRecipe(data.meals[0])
             })
             .catch((err) => {
@@ -27,6 +38,11 @@ function Recipes() {
                 return res.json()
             }) 
                 .then((data) => {
+                    // The below code isn't required because the image has already loaded on intial render from the useEffect, therefore old images act as a placeholder.  Fine for now.
+                    // let newData = data.meals[0]
+                    // if (counter.current <= newData.idMeal) {
+                    //     setLoading(false);
+                    // }
                 setRecipe(data.meals[0])
             })
                 .catch((err) => {
@@ -37,12 +53,14 @@ function Recipes() {
 
         <div>
             <button onClick={handleClick}>Another Recipe!</button>
-            <ImageContainer
-                img={recipe.strMealThumb}
-                video={recipe.strYoutube}
-                msg={recipe.strMeal} 
-                instructions={recipe.strInstructions} 
-            />
+            <div id='recipe-image-container' style={{visibility: loading ? "hidden" : "visible"}}>
+                <ImageContainer
+                    img={recipe.strMealThumb}
+                    video={recipe.strYoutube}
+                    msg={recipe.strMeal} 
+                    instructions={recipe.strInstructions}             
+                />
+            </div>
         </div>
     )
 }
