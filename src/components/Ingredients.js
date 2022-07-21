4a66946c78270f1b04d7835ab145f1d04c5c4e1b
jromatowski <joshuaromatowski@gmail.com>
2022-07-20 19:56:08 -0700

    auto populating a list now

--- src/components/Ingredients.js
+++ src/components/Ingredients.js
@@ -4,7 +4,7 @@ function Ingredients() {
 
     const url = "https://www.themealdb.com/api/json/v1/1/random.php"
 
-    const [ingredientList, setIngredientList] = useState('')
+    const [ingredientList, setIngredientList] = useState([])
 
     useEffect(() => {    
         fetch(url)
@@ -12,36 +12,34 @@ function Ingredients() {
                 return res.json()
             }) 
             .then((data) => {
-                // console.log(data)
+
+                let ingredientArray = []
                 let mainArray = data.meals[0]
-                // console.log("strIngredient"+'1')
-                // console.log(mainArray)
-                // console.log(mainArray[0].strIngredient1)
                 for (let i=1; i<21; i+=1) {
-                    let blankArray = "strIngredient"+i
-                    // console.log(blankArray)
-                    // console.log(mainArray[0])
-                    console.log(mainArray[blankArray])
-                    // for (let i=1; i<21; i+=1) {
-                    //     return mainArray[0].blankArray
-                    // }
+                    let ingredientString = "strIngredient"+i
+                    // console.log(mainArray[ingredientString])
+                    ingredientArray.push(mainArray[ingredientString])
+                    // console.log(ingredientArray)
+                    setIngredientList(ingredientArray)
                 }
-                // console.log(mainArray[0].strIngredient1)
-                // let filteredArray = mainArray.filter((ingredient) => {
-                //     for (let i=1; i < 21; i+=1)
-                //         return "mainArray.strIngredient" + i
-                // })
-            })
-            
+            })           
             .catch((err) => {
                 console.log(err);
             })
 }, [])
 
-    // console.log(ingredientList[0])
+console.log(ingredientList)
 
     return (
-        <div>Ingredients Components</div>
+        <>
+            <div>Ingredients Components</div>
+            <ul>
+                {ingredientList.map((ingredientItem) => (
+                    <ul>{ingredientItem}</ul>
+                ))}
+            </ul>
+        </>
+
     )
 }
 
