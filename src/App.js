74fd7b219329b722759f477454caa09f2080a48d
jromatowski <joshuaromatowski@gmail.com>
2022-07-20 17:30:43 -0700

    more css stuff

--- src/App.js
+++ src/App.js
@@ -19,7 +19,7 @@ function App() {
       <header>
         <h1 className='navigation' >
           <Link to="/">home</Link>
-          <Link to='/recipes'>~recipe</Link>
+          <Link to='/recipes'>|RECIPE</Link>
           {/* <Link to='/breakfast'>am.</Link>
           <Link to='/lunch'>lunch.</Link>
           <Link to='/dinner'>pm</Link> */}
